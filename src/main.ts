7ab15d29390bf26a8313d5251b6b5741821794e7
Aldair0308 <al222111405@gmail.com>
2024-07-14 01:17:24 -0600

    encorns methods enable

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,29 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConnectionOptions } from 'typeorm';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Opcionalmente, define aquí el origen específico o '*' para permitir desde cualquier origen
+    methods: 'GET, POST, PATCH, PUT, DELETE', // Incluye PATCH en los métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-
     }),
   );
 
-
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
-    console.log(`Application is running on: ${await app.getUrl()}`);
-
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();

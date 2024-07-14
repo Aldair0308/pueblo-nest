6359a7afb160d8b10a388d32a0fbe29dab1b47a1
Aldair0308 <al222111405@gmail.com>
2024-07-13 19:01:10 -0600

    main funcion out

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './database.config'; // Asegúrate de que esto apunte a tu archivo de configuración
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.use(
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'monorail.proxy.rlwy.net',
-      port: 58781,
-      username: 'root',
-      password: 'btFtQPcgTDtngJwgZNbHOvgdUzNXBsXJ',
-      database: 'railway',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Solo en desarrollo, sincroniza automáticamente el esquema de la base de datos
-    }),
-  );
+  // Configura TypeORM con la configuración de la base de datos
+  await app.use(TypeOrmModule.forRoot(config));
 
-  // Configurar el puerto desde la variable de entorno, si está definida
+  // Configura el puerto desde la variable de entorno, si está definida
   const port = process.env.PORT || 3000;
 
   await app.listen(port);

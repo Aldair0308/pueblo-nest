b3d3dcd48d1fd16b07f599609d7b5ec4613ccd2e
Aldair0308 <al222111405@gmail.com>
2024-07-13 21:05:04 -0600

    database config updated

--- src/database.config.ts
+++ src/database.config.ts
@@ -4,11 +4,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'monorail.proxy.rlwy.net',
-  port: 58781,
-  username: 'root',
-  password: 'btFtQPcgTDtngJwgZNbHOvgdUzNXBsXJ',
-  database: 'railway',
+  host: process.env.DB_HOST || 'monorail.proxy.rlwy.net', // Usar variable de entorno o un valor por defecto
+  port: parseInt(process.env.DB_PORT, 10) || 58781, // Usar variable de entorno o un valor por defecto
+  username: process.env.DB_USERNAME || 'root', // Usar variable de entorno o un valor por defecto
+  password: process.env.DB_PASSWORD || 'btFtQPcgTDtngJwgZNbHOvgdUzNXBsXJ', // Usar variable de entorno o un valor por defecto
+  database: process.env.DB_DATABASE || 'railway', // Usar variable de entorno o un valor por defecto
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true, // Solo en desarrollo, sincroniza automáticamente el esquema de la base de datos
 };

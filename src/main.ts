31634c0d047d3d9da39b99143953c3099ff33454
Aldair0308 <al222111405@gmail.com>
2024-07-13 22:12:39 -0600

    Main Try Three

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConnectionOptions } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = {
-    type: 'mysql',
-    host: process.env.DB_HOST || 'monorail.proxy.rlwy.net',
-    port: parseInt(process.env.DB_PORT, 10) || 58781,
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'btFtQPcgTDtngJwgZNbHOvgdUzNXBsXJ',
-    database: process.env.DB_DATABASE || 'railway',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-  };
+  try {
+    await app.use(TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'monorail.proxy.rlwy.net',
+      port: parseInt(process.env.DB_PORT, 10) || 58781,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'btFtQPcgTDtngJwgZNbHOvgdUzNXBsXJ',
+      database: process.env.DB_DATABASE || 'railway',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }));
 
-  await app.use(TypeOrmModule.forRoot(config));
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`Application is running on: ${await app.getUrl()}`);
+  } catch (error) {
+    console.error('Error starting the application:', error);
+  }
 }
 
 bootstrap();

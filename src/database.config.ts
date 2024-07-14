700e3eaf397346fbcd91e5147acd5fc166c3bfa0
Aldair0308 <al222111405@gmail.com>
2024-07-13 20:12:19 -0600

    confi flase

--- src/database.config.ts
+++ src/database.config.ts
@@ -10,7 +10,7 @@ const config: TypeOrmModuleOptions = {
   password: 'btFtQPcgTDtngJwgZNbHOvgdUzNXBsXJ',
   database: 'railway',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true, // Solo en desarrollo, sincroniza automáticamente el esquema de la base de datos
+  synchronize: false, // Solo en desarrollo, sincroniza automáticamente el esquema de la base de datos
 };
 
 export default config;

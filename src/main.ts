6ae1521f89c03a7edebd2eda123f9acac6183e0b
Aldair0308 <al222111405@gmail.com>
2024-07-13 18:09:10 -0600

    Database conexion

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './database.config'; // Importa la configuración de la base de datos
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  // Configura TypeORM con la configuración de la base de datos
+  app.use(await TypeOrmModule.forRoot(config));
+
   // Configurar el puerto desde la variable de entorno, si está definida
   const port = process.env.PORT || 3000;
 

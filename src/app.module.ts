00a93b2a6c6f42b0931f2cee893696e4f71c6603
Aldair0308 <al222111405@gmail.com>
2024-07-14 01:50:44 -0600

    Rondas Productos Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MesasModule } from './mesas/mesas.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductosModule } from './productos/productos.module';
+import { RondasModule } from './rondas/rondas.module';
 
 @Module({
   imports: [
@@ -14,7 +16,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'railway',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // Solo en desarrollo, sincroniza automáticamente el esquema de la base de datos
-    })
+    }),
+    ProductosModule,
+    RondasModule
 
   ],
   controllers: [],
